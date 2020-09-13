8c84cd1939ef7205d96cd648e5bb829dae7e319f
Dan Kersten <danielytics@users.noreply.github.com>
2020-09-13 02:27:04 +0100

    New wayland config

--- screenlayout/multi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-setxkbmap -model pc105 -layout us -variant euro -option
-setxkbmap -model pc105 -layout us -variant euro -option ctrl:nocaps
-# xrandr --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --mode 2560x1440 --pos 1920x0 --rotate normal --output VIRTUAL1 --off
--- screenlayout/single.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-setxkbmap -model pc105 -layout us -variant colemak
-setxkbmap -model pc105 -layout us -variant colemak -option ctrl:nocaps
-xrandr --output HDMI1 --off --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
